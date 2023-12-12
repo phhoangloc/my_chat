49281d97979a4a6b06d15c27a803a77a754fffdf
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2023-12-12 19:00:43 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,138 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+'use client'
+import { useState, useEffect, useRef } from 'react'
+import { io } from "socket.io-client"
+import Peer from 'peerjs'
+import { myWebcam, yourWebcam } from '@/function/webcam'
 export default function Home() {
+
+
+  const [socket, setSocket] = useState<any>()
+
+  const [notice, setNotice] = useState<string>("")
+
+  const [name, setName] = useState<string>("")
+  const [room, setRoom] = useState<string>("one")
+
+  const [msg, setMsg] = useState<string>("")
+  const [receivedMsg, setReceiverMsg] = useState<any>()
+  const [arrayMsg, setArrayMsg] = useState<any>([])
+
+  const [inRoom, setInRoom] = useState<boolean>(false)
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef2 = useRef<HTMLVideoElement>(null);
+  const [videocallOpen, setVideoCallOpen] = useState<boolean>(false)
+
+
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [clientstream, setClientStream] = useState<MediaStream | null>(null);
+  useEffect(() => {
+    const socket = io("http://localhost:3010/")
+    setSocket(socket)
+    socket.on("messageMe", (data: any) => {
+      if (data.type === "note") {
+        setNotice(data.msg + " " + data.room)
+        setTimeout(() => {
+          setNotice("")
+        }, 3000)
+      }
+      if (data.type === "msg") {
+        setReceiverMsg(data)
+      }
+    })
+
+    socket.on("messageYou", (data) => {
+      if (data.type === "note") {
+        setNotice(data.name + " " + data.msg)
+        setTimeout(() => {
+          setNotice("")
+        }, 3000)
+      }
+      if (data.type === "msg") {
+        setReceiverMsg(data)
+      }
+    })
+
+    socket.on("webYou", async (data) => {
+      let packagedData = JSON.parse(data.myStream);
+      const newStream = await yourWebcam(packagedData)
+
+      videoRef2 && videoRef2.current ? videoRef2.current.srcObject = newStream : null
+      setClientStream(newStream)
+      setVideoCallOpen(true)
+
+    })
+
+    return () => { socket.disconnect() }
+  }, [])
+
+  useEffect(() => {
+    receivedMsg && setArrayMsg([...arrayMsg, receivedMsg])
+  })
+
+  const enterRoom = () => {
+    setInRoom(true)
+    socket.emit("enter", { name, room })
+  }
+
+  const leaveRoom = () => {
+    setInRoom(false)
+    socket.emit("leave", { name, room })
+  }
+
+  const sendMsg = () => {
+    msg.length && socket.emit("send msg", { name, room, msg })
+    setMsg("")
+  }
+
+  const openWebcam = async () => {
+    const currentStream: MediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    const myStream = myWebcam(currentStream)
+    socket.emit("onWebcam", { myStream, room })
+    videoRef && videoRef.current ? videoRef.current.srcObject = currentStream : null
+    setStream(currentStream)
+    setVideoCallOpen(true)
+  }
+
+  const closeWebcam = async () => {
+    const tracks = stream?.getTracks();
+    tracks && tracks.forEach((track) => track.stop());
+    // setVideoCallOpen(false)
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+    <main>
+      {!inRoom ? <div className="enterPath">
+        <h1>welcome to my chat</h1>
+        <input placeholder='username' value={name} onChange={(e) => setName(e.target.value)}></input><br></br>
+        <p>room : <select placeholder='room' value={room} onChange={(e) => setRoom(e.target.value)}>
+          <option>one</option>
+          <option>two</option>
+          <option>three</option>
+        </select>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <br></br>
+        {name && <button onClick={() => enterRoom()}>enter</button>}
+      </div> :
+        <div className="roomPath">
+          <p>room: {room} <button onClick={() => leaveRoom()}>leave</button></p>
+          <div className="msgBox">
+            <p>{notice}</p>
+            <button onClick={() => openWebcam()}>webcam</button>
+            {arrayMsg.map((item: any, index: number) => <p className={`item ${item.sender ? "sender" : ""}`} key={index}><span>{item.name}</span><br></br>{item.msg}</p>)}
+          </div>
+          <div className="inputBox">
+            <textarea placeholder='message' value={msg} onChange={(e) => setMsg(e.target.value)}></textarea>
+            <button onClick={() => sendMsg()}>send</button>
+          </div>
+
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      }
+      <div className={`videocall ${videocallOpen ? "videocallOpen" : ""}`}>
+        <button onClick={() => closeWebcam()}>close webcam</button>
+        <video ref={videoRef} autoPlay playsInline />
+        <video ref={videoRef2} autoPlay playsInline />
       </div>
     </main>
   )
---
+++ src/server.js
@@ -0,0 +1,54 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const server = express();
+const httpServer = http.createServer(server);
+const io = new Server(httpServer);
+
+app.prepare().then(() => {
+    server.get('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    io.on('connection', (socket) => {
+        socket.emit("messageMe", { type: "note", msg: "welcome to my chat" })
+
+
+        socket.on("enter", (data) => {
+            socket.name = data.name
+            socket.join(data.room)
+            socket.emit("messageMe", { type: "note", ...data, msg: "welcome to " })
+            socket.in(data.room).emit("messageYou", { type: "note", ...data, msg: " enter room " })
+        })
+
+        socket.on("leave", (data) => {
+            socket.leave(data.room);
+            socket.in(data.room).emit("messageYou", { type: "note", ...data, msg: " leave room " })
+        })
+
+        socket.on("onWebcam", (data) => {
+            socket.emit("webMe", data)
+            socket.in(data.room).emit("webYou", data)
+        })
+
+        socket.on("send msg", (data) => {
+            socket.emit("messageMe", { type: "msg", sender: true, ...data })
+            socket.in(data.room).emit("messageYou", { type: "msg", sender: false, ...data })
+        })
+
+        socket.on('disconnect', () => {
+            socket.broadcast.emit("messageYou", { type: "note", name: socket.name, msg: " leave room " })
+        });
+    });
+
+    httpServer.listen(3010, () => {
+        console.log('> Ready on http://localhost:3010');
+    });
+});
